f10174b990c7e05efb498cad95a17bb05d657c30
anoop <anoopyadavan@gmail.com>
2020-05-03 16:34:47 +0530

    caesar cipher
    adding code in caesar cipher decryption

--- caesar/caesar_decryption.py
+++ caesar/caesar_decryption.py
@@ -1,1 +1,27 @@
 # e(x) = (x-k) % 26 where x is alphabet and k is shift key
+
+plain_text=input("Enter Plain Text: ")
+print("Shift by\t\t","Decrypted data")
+for shift in range(27):
+    num=97
+    print("---------------------------\n",shift,":\t\t", end="")
+    for i  in plain_text:
+        if ord(i) >= 97 and ord(i) <=122 :
+            var=((ord(i)-97)-shift) % 26
+            alpha=97
+            for ch in range(0,26):
+                if var == ch:
+                    print(chr(alpha), end="")
+                else:
+                    alpha+=1
+        elif ord(i) >= 65 and ord(i) <=90 :
+            var=((ord(i)-65)-shift) % 26
+            alpha=65
+            for ch in range(0,26):
+                if var == ch:
+                    print(chr(alpha), end="")
+                else:
+                    alpha+=1
+        else:
+            print(i, end="")
+    print()
--- caesar/caesar_encryption.py
+++ caesar/caesar_encryption.py
@@ -2,25 +2,27 @@
 # character+key in integer add it and take modulus and then make character
 
 plain_text=input("Enter Plain Text: ")
-shift=int(input("Enter shift key: "))
-num=97
-for i  in plain_text:
-    if ord(i) >= 97 and ord(i) <=122 :
-        var=((ord(i)-97)+shift) % 26
-        alpha=97
-        for ch in range(0,26):
-            if var == ch:
-                print(chr(alpha), end="")
-            else:
-                alpha+=1
-    elif ord(i) >= 65 and ord(i) <=90 :
-        var=((ord(i)-65)+shift) % 26
-        alpha=65
-        for ch in range(0,26):
-            if var == ch:
-                print(chr(alpha), end="")
-            else:
-                alpha+=1
-    else:
-        print(i, end="")
-print()
+print("Shift by\t\t","Encrypted data")
+for shift in range(27):
+    num=97
+    print("---------------------------\n",shift,":\t\t", end="")
+    for i  in plain_text:
+        if ord(i) >= 97 and ord(i) <=122 :
+            var=((ord(i)-97)+shift) % 26
+            alpha=97
+            for ch in range(0,26):
+                if var == ch:
+                    print(chr(alpha), end="")
+                else:
+                    alpha+=1
+        elif ord(i) >= 65 and ord(i) <=90 :
+            var=((ord(i)-65)+shift) % 26
+            alpha=65
+            for ch in range(0,26):
+                if var == ch:
+                    print(chr(alpha), end="")
+                else:
+                    alpha+=1
+        else:
+            print(i, end="")
+    print()
